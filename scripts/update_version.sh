37bcfd84d9f99bfef7d7c7f3a23278527ac7e1db
artkonr <artkonr@gmail.com>
2020-07-31 20:20:40 +0300

    script update

--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -3,9 +3,7 @@
 URL="https://raw.githubusercontents.com/artkonr/shell-scripts/master/rust/update_version.sh"
 
 touch update_script.sh
-
 curl "$URL" > update_script.sh
-
 ./update_script.sh
 
 rm update_script.sh
