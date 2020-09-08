54491ce96c2ffc4081260f347e445cfd736e1960
artkonr <artkon-r@yandex.ru>
2020-09-08 13:23:23 +0300

    Update script adjusted to work with MacOS

--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -1,9 +1,21 @@
 # use update_version.sh from shell-scripts
 
-URL="https://raw.githubusercontents.com/artkonr/shell-scripts/master/rust/update_version.sh"
+SCRIPT_PATH="https://raw.githubusercontent.com/artkonr/shell-scripts/master/rust"
 
+# define OS and construct appropriate file name
+OSN=$(uname -s)
+MACHINE=""
+case $OSN in
+  Linux*) MACHINE="linux";;
+  Darwin*) MACHINE="mac";;
+  *) MACHINE="Unknown machine $OSN"
+esac
+SCRIPT_PATH="$SCRIPT_PATH/update_version_$MACHINE.sh"
+
+# download the script and apply it
 touch update_script.sh
-curl "$URL" > update_script.sh
+curl "$SCRIPT_PATH" > update_script.sh
+chmod +x ./update_script.sh
 ./update_script.sh
 
 rm update_script.sh
